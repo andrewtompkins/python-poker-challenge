c428db37deaed1801bded9508b9350cd62121fb2
Andrew Tompkins <andy.tompkins@dimensiondata.com>
2024-02-13 16:31:04 +0200

    Implemented Deck.pick() function and added tests for picking number of cards exceeding the deck size

--- pokerhands/deck.py
+++ pokerhands/deck.py
@@ -1,12 +1,13 @@
 import random
-
 from .suit import Suit
 from .rank import Rank
 from .card import Card
 
-
 class Deck:
+
     def __init__(self):
+        self.initial_deck_size = 52
+        self.number_of_cards_left = self.initial_deck_size;
         self._cards = []
         for suit in Suit:
             for rank in Rank:
@@ -14,14 +15,20 @@ class Deck:
         random.shuffle(self._cards)
 
     def number_of_cards(self):
-        return len(self._cards)
+        return self.number_of_cards_left
+    
+    def get_initial_deck_size(self):
+        return self.initial_deck_size
 
     def pick(self, number_of_cards):
-        raise NotImplementedError("You need to implement this")
-
+        if number_of_cards > self.number_of_cards_left:
+            raise NotEnoughCardsException("There are not enough cards in the deck. The Deck contains " + str(self.number_of_cards_left) + " and " + str(number_of_cards) + " were requested")
+        else:
+            picked_cards = self._cards[:min(number_of_cards, len(self._cards))]
+            self.number_of_cards_left -= len(picked_cards)
+            return picked_cards
 
 class NotEnoughCardsException(Exception):
     def __init__(self, message):
         super().__init__(message)
-
 
--- tests/deck_test.py
+++ tests/deck_test.py
@@ -1,7 +1,6 @@
 import unittest
 from pokerhands.deck import Deck, NotEnoughCardsException
 
-
 class DeckTest(unittest.TestCase):
     def test_deck_size(self):
         deck = Deck()
@@ -25,6 +24,21 @@ class DeckTest(unittest.TestCase):
         self.assertEqual(52, len(cards))
         self.assertEqual(0, deck.number_of_cards())
 
+    def test_multiple_picks_balance(self):
+        deck = Deck()
+
+        # Ensure pick works when there are enough cards
+        cards = deck.pick(5)
+        self.assertEqual(len(cards), 5)
+        cards = deck.pick(3)
+        self.assertEqual(len(cards), 3)
+        self.assertEqual(deck.number_of_cards(), deck.get_initial_deck_size() - 8)  # Ensure number of remaining cards is correct
+
+    def test_pick_too_many_cards(self):
+        deck = Deck()
+        cards = deck.pick(52)
+        with self.assertRaises(NotEnoughCardsException):
+            deck.pick(3)  # Try to pick more cards than available
 
 if __name__ == '__main__':
     unittest.main()
